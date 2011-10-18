53146c55b49e77786baa88e53b65cfc4e94d8ac0
Tanguy de Courson <tanguy@0x7a69.com>
2011-10-18 13:20:47 -0400

    up the version number to 1.0.0

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_periodictask do
   name 'Redmine Periodictask plugin'
   author 'Tanguy de Courson'
   description 'This is a plugin for Redmine that will allow you to schedule a task to be assigned on a schedule'
-  version '0.1.0'
+  version '1.0.0'
 
   permission :periodictask, {:periodictask => [:index, :edit]}, :public => true
   menu :project_menu, :periodictask, { :controller => 'periodictask', :action => 'index' }, :caption => 'Periodic Task', :after => :settings, :param => :project_id
