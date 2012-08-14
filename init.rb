5979a61b54ded747031f6c2b5ab6ee31a846e721
csfreak <github@csfreak.com>
2012-08-14 18:13:27 -0500

    Created Module
    Created a module for periodic tasks and moved permission to that module.  Also removed public permission so module permission can be enforced.

--- init.rb
+++ init.rb
@@ -6,6 +6,9 @@ Redmine::Plugin.register :redmine_periodictask do
   description 'This is a plugin for Redmine that will allow you to schedule a task to be assigned on a schedule'
   version '1.0.0'
 
-  permission :periodictask, {:periodictask => [:index, :edit]}, :public => true
+  project_module :periodictask do
+    permission :periodictask, {:periodictask => [:index, :edit]}
+  end
+
   menu :project_menu, :periodictask, { :controller => 'periodictask', :action => 'index' }, :caption => 'Periodic Task', :after => :settings, :param => :project_id
 end
