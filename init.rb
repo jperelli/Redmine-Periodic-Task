5addb525202303b63a6a8481bffc8160a57d8f2e
jperelli <jperelli@gmail.com>
2013-01-24 15:58:51 -0300

    Completed support to use datetime scheduling as well as the date

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_periodictask do
   name 'Redmine Periodictask plugin'
   author 'Tanguy de Courson'
   description 'This is a plugin for Redmine that will allow you to schedule a task to be assigned on a schedule'
-  version '1.0.0'
+  version '1.0.1'
 
   project_module :periodictask do
     permission :periodictask, {:periodictask => [:index, :edit]}
--- lib/scheduled_tasks_checker.rb
+++ lib/scheduled_tasks_checker.rb
@@ -1,6 +1,6 @@
 class ScheduledTasksChecker
   def self.checktasks!
-     Periodictask.all(:conditions=> ["next_run_date <= ? ", Time.now.to_date]).each do |task| 
+     Periodictask.all(:conditions=> ["next_run_date <= ? ", Time.now]).each do |task| 
        print "assigning #{task.subject}"
          issue = Issue.new(:project_id=>task.project_id,  :tracker_id=>task.tracker_id, :assigned_to_id=>task.assigned_to_id, :author_id=>task.author_id, :subject=>task.subject);
          issue.save         
