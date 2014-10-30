4a5451eb22cf7d70548ac9514f9beac1fcb50473
jperelli <jperelli@gmail.com>
2014-10-30 20:23:55 -0300

    Fixes #14: migration not working for postgres
    Fixes #14 migration not working for postgres

--- db/migrate/20140722185522_add_date_and_description_to_periodictasks.rb
+++ db/migrate/20140722185522_add_date_and_description_to_periodictasks.rb
@@ -2,7 +2,7 @@ class AddDateAndDescriptionToPeriodictasks < ActiveRecord::Migration
   def self.up
     add_column :periodictasks, :set_start_date, :boolean, :null => false, :default => false
     add_column :periodictasks, :due_date_number, :integer, :null => true, :default => nil
-    add_column :periodictasks, :due_date_units, :string, :null => false
+    add_column :periodictasks, :due_date_units, :string, :null => true
     add_column :periodictasks, :description, :text, :null => true
   end
 
