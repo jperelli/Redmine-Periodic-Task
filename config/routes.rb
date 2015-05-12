81962eb3424ce89c1c76d9969131eead43729167
Christopher Michels <s2chmich@uni-trier.de>
2015-05-12 16:14:12 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
+#  resources :periodictask
 #  es igual a las siguientes rutas
+
 #  periodictask_index GET      /periodictask(.:format)            periodictask#index#                    POST     /periodictask(.:format)            periodictask#create
 #  new_periodictask   GET      /periodictask/new(.:format)        periodictask#new
 #  edit_periodictask  GET      /periodictask/:id/edit(.:format)   periodictask#edit
@@ -8,8 +10,8 @@ Rails.application.routes.draw do
 #                     PUT      /periodictask/:id(.:format)        periodictask#update
 #                     DELETE   /periodictask/:id(.:format)        periodictask#destroy
   
-  # replaced put with match for action 'update', allowing both http-verb options 'put' 
-  # and the new verb 'patch' for compatibility with Redmine 3 and below
+#  replaced put with match for action 'update', allowing both http-verb options 'put' 
+#  and the new verb 'patch' for compatibility with Redmine 3 and below
   
   get      'projects/:project_id/periodictask',            :to => 'periodictask#index',  :as => 'periodictasks'
   get      'projects/:project_id/periodictask/new',        :to => 'periodictask#new',    :as => 'new_periodictask'
