85b18c337abe465ff46ff33a412e6c55bf6f4c4a
Julián Perelli <jperelli@gmail.com>
2015-10-22 10:03:47 -0300

    Comment minor modification

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-#  resources :periodictask
-#  es igual a las siguientes rutas
-
-#  periodictask_index GET      /periodictask(.:format)            periodictask#index#                    POST     /periodictask(.:format)            periodictask#create
-#  new_periodictask   GET      /periodictask/new(.:format)        periodictask#new
-#  edit_periodictask  GET      /periodictask/:id/edit(.:format)   periodictask#edit
-#       periodictask  GET      /periodictask/:id(.:format)        periodictask#show
-#                     PUT      /periodictask/:id(.:format)        periodictask#update
-#                     DELETE   /periodictask/:id(.:format)        periodictask#destroy
-  
+#  this is like "resources :periodictask" but it has been
 #  replaced put with match for action 'update', allowing both http-verb options 'put' 
 #  and the new verb 'patch' for compatibility with Redmine 3 and below
   
