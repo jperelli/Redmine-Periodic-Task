241ea5ae496579d8a2728f39369008ae33d95a6d
wate <pc.poisoning@gmail.com>
2018-08-01 18:51:18 +0900

    文言を調整

--- init.rb
+++ init.rb
@@ -1,16 +1,14 @@
 require 'redmine'
 
 Redmine::Plugin.register :periodictask do
-  name 'Redmine Periodictask plugin'
-  author 'Julian Perelli based on work from Tanguy de Courson'
+  name 'Redmine Periodic task plugin'
+  author 'yoshiaki tanaka'
   description 'This is a plugin for Redmine2 and 3 that will allow you to schedule a task to be assigned on a schedule'
   version '3.2.1'
-  url 'http://github.com/jperelli/Redmine-Periodic-Task/'
-  author_url 'http://jperelli.com.ar/'
-
+  url 'https://github.com/wate/redmine_periodic_task'
   project_module :periodictask do
     permission :periodictask, {:periodictask => [:index, :edit]}
   end
+  menu :project_menu, :periodictask, { :controller => 'periodictask', :action => 'index' }, caption: :project_menu_periodictask, after: :settings, param: :project_id
 
-  menu :project_menu, :periodictask, { :controller => 'periodictask', :action => 'index' }, :caption => 'Periodic Task', :after => :settings, :param => :project_id
 end
