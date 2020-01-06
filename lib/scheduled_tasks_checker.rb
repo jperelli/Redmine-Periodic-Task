63ddc55161254439f46f21ed44a6803f08c59296
gainskills <hanbaobao2005@gmail.com>
2020-01-06 22:26:23 +1300

    https://github.com/jperelli/Redmine-Periodic-Task/issues/25 , https://github.com/jperelli/Redmine-Periodic-Task/issues/79

--- lib/scheduled_tasks_checker.rb
+++ lib/scheduled_tasks_checker.rb
@@ -1,6 +1,8 @@
 class ScheduledTasksChecker
+
   def self.checktasks!
-    now = Time.now
+    Time.zone = User.current.time_zone
+    now = Time.zone.now
     Periodictask.where("next_run_date <= ? ", now).each do |task|
 
       # replace variables (set locale from shell)
@@ -18,11 +20,13 @@ class ScheduledTasksChecker
         interval = task.interval_number
         units = task.interval_units.downcase
         if units == "business_day"
-          task.next_run_date = task.interval_number.business_day.after(now)
+          next_run_date = task.interval_number.business_day.after(Time.zone.now)
         else
           interval_steps = ((now - task.next_run_date) / interval.send(units)).ceil
-          task.next_run_date += (interval * interval_steps).send(units)
+          next_run_date += (interval * interval_steps).send(units)
         end
+        next_run_date = next_run_date.change({ hour: Time.zone.now.hour.to_i, min: Time.zone.now.min.to_i, sec: Time.zone.now.sec.to_i })
+        task.next_run_date = next_run_date
       else
         msg = "Project is missing or closed"
         Rails.logger.error "ScheduledTasksChecker: #{msg}"
