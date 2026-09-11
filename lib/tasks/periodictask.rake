01e891ad9c306647f73df4d272eecd4ff851e9b3
Julian Perelli <jperelli@gmail.com>
2026-09-11 23:08:17 +0000

    rake redmine:check_periodictasks: explain when the plugin is not loaded (Debian plugins dir)
    Redmine loads plugins/*/lib/tasks/*.rake from Rails.root even when its
    plugins directory is elsewhere (Debian: /var/lib/redmine/<instance>/plugins),
    so the task failed with NameError: uninitialized constant ScheduledTasksChecker.
    Abort with the two directories and the README pointer instead. Fixes #179.

--- lib/tasks/periodictask.rake
+++ lib/tasks/periodictask.rake
@@ -9,6 +9,18 @@ Rails.configuration.active_job.queue_adapter = :inline if Rails.configuration.re
 
 namespace :redmine do
   task check_periodictasks: :environment do
+    # Redmine loads every plugins/*/lib/tasks/*.rake under Rails.root, but
+    # only registers the plugins found in its plugins directory, which can be
+    # elsewhere (Debian packages: /var/lib/redmine/<instance>/plugins).
+    unless Redmine::Plugin.installed?(:periodictask)
+      abort <<~MSG
+        The periodictask plugin is not loaded by this Redmine.
+        Its rake task was found under #{Rails.root.join('plugins')}, but Redmine loads plugins from
+        #{Redmine::PluginLoader.directory}. Move or symlink the plugin there and restart Redmine.
+        See https://github.com/jperelli/Redmine-Periodic-Task#installation
+      MSG
+    end
+
     ScheduledTasksChecker.checktasks!
   end
 end
---
+++ test/unit/check_periodictasks_rake_test.rb
@@ -0,0 +1,34 @@
+require "#{File.dirname(__FILE__)}/../test_helper"
+require 'rake'
+
+class CheckPeriodictasksRakeTest < ActiveSupport::TestCase
+  RAKEFILE = File.expand_path('../../lib/tasks/periodictask.rake', __dir__)
+
+  def setup
+    @previous_application = Rake.application
+    Rake.application = Rake::Application.new
+    Rake::Task.define_task(:environment)
+    Rake.load_rakefile(RAKEFILE)
+  end
+
+  def teardown
+    Rake.application = @previous_application
+  end
+
+  def test_runs_the_checker
+    ScheduledTasksChecker.expects(:checktasks!).once
+    Rake::Task['redmine:check_periodictasks'].invoke
+  end
+
+  def test_aborts_with_an_explanation_when_the_plugin_is_not_loaded
+    Redmine::Plugin.stubs(:installed?).with(:periodictask).returns(false)
+    ScheduledTasksChecker.expects(:checktasks!).never
+
+    _out, err = capture_io do
+      assert_raises(SystemExit) { Rake::Task['redmine:check_periodictasks'].invoke }
+    end
+    assert_match(/periodictask plugin is not loaded/, err)
+    assert_match(Regexp.escape(Redmine::PluginLoader.directory.to_s), err)
+    assert_match(%r{Redmine-Periodic-Task#installation}, err)
+  end
+end
